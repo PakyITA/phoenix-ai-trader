9df8abdf8c79d591259ff86ffe48d0a32609af00
PakyITA <pas.destefano@gmail.com>
2026-07-05 16:49:27 +0200

    Update default status path for Phoenix component

--- custom_components/phoenix/const.py
+++ custom_components/phoenix/const.py
@@ -1,6 +1,6 @@
 DOMAIN = "phoenix"
 
 CONF_STATUS_PATH = "status_path"
-DEFAULT_STATUS_PATH = "/config/www/phoenix/status.json"
+DEFAULT_STATUS_PATH = "/config/www/phoenix-ai-trader/status.json"
 
 PLATFORMS = ["sensor"]
