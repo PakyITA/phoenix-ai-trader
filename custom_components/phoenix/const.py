ce3b5c2539b086bfc071f098dcb545ae92f80b1c
PakyITA <pas.destefano@gmail.com>
2026-07-05 20:38:23 +0200

    Add trial setup constants

--- custom_components/phoenix/const.py
+++ custom_components/phoenix/const.py
@@ -5,12 +5,16 @@ CONF_START_CAPITAL = "start_capital"
 CONF_TARGET_CAPITAL = "target_capital"
 CONF_DURATION_VALUE = "duration_value"
 CONF_DURATION_UNIT = "duration_unit"
+CONF_EMAIL = "email"
+CONF_LICENSE_KEY = "activation_code"
 
 DEFAULT_DATA_DIR = "/config/phoenix-ai-trader"
 DEFAULT_START_CAPITAL = 100.0
 DEFAULT_TARGET_CAPITAL = 1000.0
 DEFAULT_DURATION_VALUE = 7
 DEFAULT_DURATION_UNIT = "days"
+DEFAULT_EMAIL = ""
+DEFAULT_ACTIVATION_CODE = ""
 
 STATUS_FILENAME = "status.json"
 SETTINGS_FILENAME = "settings.json"
