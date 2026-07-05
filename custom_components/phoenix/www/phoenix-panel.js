a7a7689eddddf8142d9ed439a343434058b3f20b
PakyITA <pas.destefano@gmail.com>
2026-07-05 19:35:08 +0200

    Update iframe source and add styles in phoenix-panel.js

--- custom_components/phoenix/www/phoenix-panel.js
+++ custom_components/phoenix/www/phoenix-panel.js
@@ -1,10 +1,15 @@
 class PhoenixAiTraderPanel extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-      <iframe
-        src="/phoenix_ai_trader/index.html?v=021"
-        style="border:0;width:100%;height:100vh;">
-      </iframe>
+      <style>
+        iframe {
+          width: 100%;
+          height: 100vh;
+          border: 0;
+        }
+      </style>
+
+      <iframe src="/local/phoenix/index.html?v=${Date.now()}"></iframe>
     `;
   }
 }
